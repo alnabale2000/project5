d516805610deedc69e83024d80ff7968b9a5fa90
Anas <alnabale2016@gmail.com>
2021-09-20 15:10:09 +0300

    Setup Project

---
+++ backend/db/schema.sql
@@ -0,0 +1,12 @@
+DROP DATABASE IF EXISTS project5;
+CREATE DATABASE project5;
+
+
+USE project5;
+
+
+
+-- INSERT INTO books (title ,author,puplished_at ,price) VALUES ("MobileLegends","Anas","28-8-2021","15");
+-- INSERT INTO books (title ,author,puplished_at ,price) VALUES ("Moneyball","hardy","28-8-2021","5");
+-- INSERT INTO books (title ,author,puplished_at ,price) VALUES ("React js","Mosh","20-8-2000","50");
+
