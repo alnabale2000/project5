8347d94acd195bec0b1c6f35e59a458b934ac628
Anas <alnabale2016@gmail.com>
2021-10-05 18:30:21 +0300

    get Categories and meals

--- backend/routes/controllers/categories.js
+++ backend/routes/controllers/categories.js
@@ -3,8 +3,6 @@ const connection = require("./../../db/db");
 
 const addCategory = (req, res) => {
     const id = req.params.id;
-    console.log("id", id);
-
     const categoryName = req.body.categoryName;
     const query = `INSERT INTO categories (categoryName,resturant_id)VALUES (?,?) ;`;
     const data = [categoryName, id];
@@ -14,6 +12,33 @@ const addCategory = (req, res) => {
     });
 };
 
+const getCategoriesByResturantId = (req, res) => {
+    const id = req.params.id;
+    const query = `
+    SELECT * FROM categories 
+    WHERE resturant_id =?;
+    `;
+    const data = [id];
+
+    connection.query(query, data, (err, result) => {
+        res.status(200).json(result);
+    });
+};
+const getMealsByCategoryId = (req, res) => {
+    const id = req.body.id;
+    const query = `
+    SELECT * FROM meals 
+    WHERE category_id=?
+    ;
+    `;
+    const data = [id];
+
+    connection.query(query, data, (err, result) => {
+        res.status(200).json(result);
+    });
+};
 module.exports = {
     addCategory,
+    getCategoriesByResturantId,
+    getMealsByCategoryId,
 };
--- backend/routes/routers/categories.js
+++ backend/routes/routers/categories.js
@@ -1,9 +1,15 @@
 const express = require("express");
 
-const { addCategory } = require("./../controllers/categories");
+const {
+    addCategory,
+    getCategoriesByResturantId,
+    getMealsByCategoryId,
+} = require("./../controllers/categories");
 
 const categoriesRouter = express.Router();
 
 categoriesRouter.post("/addCategory/:id", addCategory);
+categoriesRouter.get("/categories/:id", getCategoriesByResturantId);
+categoriesRouter.get("/resturants/categories/meals", getMealsByCategoryId);
 
 module.exports = categoriesRouter;
