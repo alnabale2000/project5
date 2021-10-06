4b5dcbf33c31ae3f58bdf89d3bfc1a8c8077362e
Anas <alnabale2016@gmail.com>
2021-10-06 22:21:35 +0300

    delete and get address

--- backend/routes/controllers/users_address.js
+++ backend/routes/controllers/users_address.js
@@ -17,6 +17,27 @@ const addAddress = (req, res) => {
     });
 };
 
+const deleteAddress = (req, res) => {
+    const id = req.params.id;
+    const query = `DELETE FROM users_address WHERE userId=?;`;
+    const data = [id];
+    connection.query(query, data, (err, result) => {
+        if (err) res.json(err);
+        res.json("Address Deleted");
+    });
+};
+
+const getAddressByUserId = async (req, res) => {
+    const id = req.params.id;
+    const query = `SELECT * FROM users_address WHERE userId = ?;`;
+    const data = [id];
+    const getAddress = await connection.promise().query(query, data);
+    if (!getAddress) return res.status(404).json(err);
+    res.status(200).json(getAddress[0]);
+};
+
 module.exports = {
     addAddress,
+    deleteAddress,
+    getAddressByUserId,
 };
--- backend/routes/routers/users_address.js
+++ backend/routes/routers/users_address.js
@@ -1,9 +1,11 @@
 const express = require("express");
 
-const { addAddress } = require("./../controllers/users_address");
+const { addAddress, deleteAddress, getAddressByUserId } = require("./../controllers/users_address");
 
 const usersAddressRouter = express.Router();
 
 usersAddressRouter.post("/addAddress/:id", addAddress);
+usersAddressRouter.delete("/deleteAddress/:id", deleteAddress);
+usersAddressRouter.get("/getAddress/:id", getAddressByUserId);
 
 module.exports = usersAddressRouter;
